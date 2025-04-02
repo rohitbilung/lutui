14e2271ab6a485c98285e2935f63e1821bafa4c9
Godson <godson@incrediblevisibility.com>
2025-04-02 20:43:50 +0530

    updated Product details page ui with dynamic data

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -35,190 +35,188 @@ module.exports = {
             ],
             "category": "T-Shirts",
             "description": "Powdered dry moringa leaves, commonly known as Munga Guna , is an Adivasi delicacy in and around Chota-Nagpur region, which is rich in nutrients and earthy flavors. The phrase “Roj Roj Guna Tiyan” originates from a popular Sadri folk song, typically sung during the vibrant Sailo Dance. In this melodious tune, a younger brother playfully complains to his elder brother, expressing his longing for variety in their meals. He humorously requests that his sister-in-law be persuaded to cook something other than Guna Tiyan every day, craving the indulgence of a hearty non-vegetarian dish.",
-            "sizeType": [
-                {
-                    "regular": {
-                        "price": 564,
-                        "sizes": [
-                            {
-                                "size": "S",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 10
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 15
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 12
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "M",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "L",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "XL",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "XXL",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    "oversized": {
-                        "price": 664,
-                        "sizes": [
-                            {
-                                "size": "S",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 5
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 6
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "M",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "L",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "XL",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            },
-                            {
-                                "size": "XXL",
-                                "colors": [
-                                    {
-                                        "color": "red",
-                                        "count": 8
-                                    },
-                                    {
-                                        "color": "black",
-                                        "count": 20
-                                    },
-                                    {
-                                        "color": "white",
-                                        "count": 15
-                                    }
-                                ]
-                            }
-                        ]
-                    }
+            "sizeType": {
+                "regular": {
+                    "price": 564,
+                    "sizes": [
+                        {
+                            "size": "S",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 10
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 15
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 12
+                                }
+                            ]
+                        },
+                        {
+                            "size": "M",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        },
+                        {
+                            "size": "L",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        },
+                        {
+                            "size": "XL",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        },
+                        {
+                            "size": "XXL",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        }
+                    ]
+                },
+                "oversized": {
+                    "price": 664,
+                    "sizes": [
+                        {
+                            "size": "S",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 5
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 6
+                                }
+                            ]
+                        },
+                        {
+                            "size": "M",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        },
+                        {
+                            "size": "L",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        },
+                        {
+                            "size": "XL",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        },
+                        {
+                            "size": "XXL",
+                            "colors": [
+                                {
+                                    "color": "red",
+                                    "count": 8
+                                },
+                                {
+                                    "color": "black",
+                                    "count": 20
+                                },
+                                {
+                                    "color": "white",
+                                    "count": 15
+                                }
+                            ]
+                        }
+                    ]
                 }
-            ]	
+            }
         }
         if(true){
             res.status(200).send({
