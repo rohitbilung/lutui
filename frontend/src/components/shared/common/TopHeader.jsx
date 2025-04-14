3f432c0c15cea1920483e12a907a68fb30f667b6
Godson <godson@incrediblevisibility.com>
2025-04-14 20:04:24 +0530

    added logout option for topheader

--- frontend/src/components/shared/common/TopHeader.jsx
+++ frontend/src/components/shared/common/TopHeader.jsx
@@ -1,6 +1,6 @@
 import { User } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,43 +9,23 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useAuth } from "../../../context/AuthContext";
 import UserAvatar from "./UserAvatar";
+import { useCart } from "../../../context/CartContext";
 
 const TopHeader = () => {
-  const { user } = useAuth();
+  const { clearCart } = useCart()
+  const { user, logout } = useAuth();
+
+  const handleLogout = () => {
+    clearCart();
+    logout();
+  };
+
   return (
     <div className="bg-black text-white py-3 px-2 w-full">
       <div className="w-full flex justify-center items-center text-center relative">
         <p className="text-sm flex-1 flex items-center justify-center text-center gap-2">
           Wearing Adivasi Narratives and Identity.
         </p>
-
-        {/* <div className="absolute right-0">
-          <div className="flex justify-end text-right">
-            <div className="flex items-center space-x-4">
-              <Button variant="ghost" className="md:hidden p-2">
-                <User className="w-5 h-5 text-white" />
-              </Button>
-
-              <div className="hidden md:flex space-x-4 text-sm">
-                {user ? (
-                  <UserAvatar />
-                ) : (
-                  <>
-                    <Link to="/login" className="hover:text-blue-500 uppercase">
-                      Login
-                    </Link>
-                    <Link
-                      to="/register"
-                      className="hover:text-blue-500 uppercase"
-                    >
-                      Create Account
-                    </Link>
-                  </>
-                )}
-              </div>
-            </div>
-          </div>
-        </div> */}
 
         <div className="absolute right-0">
           <div className="flex justify-end text-right">
@@ -87,7 +67,7 @@ const TopHeader = () => {
                           </>
                         )}
                         <DropdownMenuItem asChild>
-                          <button className="w-full text-left cursor-pointer">
+                          <button className="w-full text-left cursor-pointer" onClick={handleLogout}>
                             Logout
                           </button>
                         </DropdownMenuItem>
