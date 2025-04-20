07e52e5ebfa4b802b1ccfff94b702570b5cda42b
Godson <godson@incrediblevisibility.com>
2025-04-20 23:03:51 +0530

    Fixed addToCart function

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -80,70 +80,75 @@ export const CartProvider = ({ children }) => {
   }, [cart]);
 
   const totalPrice = useMemo(() => {
-    const sum = cart ? cart.reduce((acc, i) => acc + i.price * i.quantity, 0) : 0;
+    const sum = cart
+      ? cart.reduce((acc, i) => acc + i.price * i.quantity, 0)
+      : 0;
     return sum;
   }, [cart]);
 
   const isInCart = (product) => {
-    return cart && cart.some(
-      (item) =>
-        item.productId._id === product.productId._id &&
-        item.color === product.color &&
-        item.size === product.size &&
-        item.type === product.type &&
-        item.quantity > 0
+    return (
+      cart &&
+      cart.some(
+        (item) =>
+          item.productId._id === product.productId._id &&
+          item.color === product.color &&
+          item.size === product.size &&
+          item.type === product.type &&
+          item.quantity > 0
+      )
     );
   };
 
   const getItemQuantity = (product) => {
-    const item = cart && cart.find(
-      (item) =>
-        item.productId._id === product.productId._id &&
-        item.color === product.color &&
-        item.size === product.size &&
-        item.type === product.type
-    );
+    const item =
+      cart &&
+      cart.find(
+        (item) =>
+          item.productId._id === product.productId._id &&
+          item.color === product.color &&
+          item.size === product.size &&
+          item.type === product.type
+      );
     return item ? item.quantity : 0;
   };
 
   const addToCart = async (item) => {
     if (!user) {
-      setCart((prevCart) => {
-        console.log("prevCart: ", prevCart, " ; typeof prevCart: ", typeof prevCart)
-        const existingIndex = prevCart.findIndex(
-          (product) =>
-            product.productId._id === item.productId._id &&
-            product.type === item.type &&
-            product.color === item.color &&
-            product.size === item.size
-        );
+      const existingIndex = cart.findIndex(
+        (product) =>
+          product.productId._id === item.productId._id &&
+          product.type === item.type &&
+          product.color === item.color &&
+          product.size === item.size
+      );
 
-        let updatedCart;
-        if (existingIndex !== -1) {
-          // Item exists, update quantity
-          updatedCart = [...prevCart];
-          updatedCart[existingIndex].quantity += 1;
-          return updatedCart;
-        } else {
-          // Item does not exist, add it
-          updatedCart = [...prevCart, { ...item, quantity: 1 }];
-        }
-        saveCartToLocalStorage(updatedCart); // ✅ Save to localStorage
-        return updatedCart;
-      });
+      let updatedCart = [];
+      if (existingIndex !== -1) {
+        // Item exists, update quantity
+        updatedCart = [...cart];
+        updatedCart[existingIndex].quantity += 1;
+      } else {
+        // Item does not exist, add it
+        updatedCart = [...prevCart, { ...item, quantity: 1 }];
+      }
+      saveCartToLocalStorage(updatedCart); // ✅ Save to localStorage
+      setCart([...updatedCart]);
       return;
-    }
-    const reqObject = { ...item, quantity: 1, productId: item.productId._id };
-    // calculate total from all products
-    const totalPrice =
-      cart.reduce((acc, i) => acc + i.price * i.quantity, 0) + reqObject.price;
-    const cartObject = { userId, ...reqObject, totalPrice };
-    const response = await addItemToCart(cartObject);
-    if (response.success) {
-      toast.success(response.message);
-      refetch();
     } else {
-      toast.error(response.message);
+      const reqObject = { ...item, quantity: 1, productId: item.productId._id };
+      // calculate total from all products
+      const totalPrice =
+        cart.reduce((acc, i) => acc + i.price * i.quantity, 0) +
+        reqObject.price;
+      const cartObject = { userId, ...reqObject, totalPrice };
+      const response = await addItemToCart(cartObject);
+      if (response.success) {
+        toast.success(response.message);
+        refetch();
+      } else {
+        toast.error(response.message);
+      }
     }
   };
 
